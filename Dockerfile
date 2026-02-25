fa34e800d09ec503fdba002662d83c861f2b53d2
krafto-o <kv@peektech.cc>
2026-02-25 06:15:52 -0600

    Carga del entorno de desarrollo

---
+++ Dockerfile
@@ -0,0 +1,8 @@
+FROM php:8.3-apache
+
+RUN docker-php-ext-install pdo pdo_mysql
+
+RUN a2enmod rewrite
+
+RUN chown -R www-data:www-data /var/www/html
+
---
+++ src/index.php
@@ -0,0 +1,15 @@
+<?php
+$host = 'db'; // Nombre del servicio en docker-compose
+$db   = 'siped';
+$user = 'root';
+$pass = 'root';
+
+try {
+    $pdo = new PDO("mysql:host=$host;dbname=$db", $user, $pass);
+    echo "<h1>¡Conexión Exitosa!</h1>";
+    echo "<p>El sistema de la consulta pediátrica ya se habla con la base de datos MariaDB.</p>";
+} catch (PDOException $e) {
+    echo "<h1>Error de conexión</h1>";
+    echo "<p>Detalle: " . $e->getMessage() . "</p>";
+}
+?>
