f0d6689e0e59683d08a83f3fe545e8b30a0f9224
Gabicancino <gabriela.cancino.maza.cetis138@gmail.com>
2026-03-20 11:54:47 -0600

    test

--- src/index.php
+++ src/index.php
@@ -7,7 +7,7 @@ $pass = 'root';
 try {
     $pdo = new PDO("mysql:host=$host;dbname=$db", $user, $pass);
     echo "<h1>¡Conexión Exitosa!</h1>";
-    echo "<p>El sistema de la consulta pediátrica ya se habla con la base de datos MariaDB.</p>";
+    echo "<p>El sistema de la consulta pediátrica ya se habla con la base de datos.</p>";
 } catch (PDOException $e) {
     echo "<h1>Error de conexión</h1>";
     echo "<p>Detalle: " . $e->getMessage() . "</p>";
